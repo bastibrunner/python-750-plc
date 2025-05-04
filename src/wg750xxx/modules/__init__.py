460dcbc9c5ff6ab03b0b19224438040faea91603
Bastian Brunner <bastian.brunner@pexon-consulting.de>
2025-05-04 18:55:11 +0200

    module exports

--- src/wg750xxx/modules/__init__.py
+++ src/wg750xxx/modules/__init__.py
@@ -8,12 +8,16 @@ from .analog.modules import (
     Wg750AnalogOut2Ch,
     Wg750AnalogOut4Ch,
 )
+from .channel import WagoChannel
 from .controller.modules import Wg750FeldbuskopplerEthernet
 from .counter.modules import Wg750Counter
 from .dali.modules import Wg750DaliMaster
 from .digital.modules import Wg750DigitalIn, Wg750DigitalOut
+from .module import WagoModule
 
 __all__ = [
+    "WagoChannel",
+    "WagoModule",
     "Wg750AnalogIn1Ch",
     "Wg750AnalogIn2Ch",
     "Wg750AnalogIn4Ch",
